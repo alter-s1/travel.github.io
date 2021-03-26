3dd9ce4c1e14eb64ef128b61cf4bca2f6c3c36fd
alter-s1 <k.s.002.a.u.a@gmail.com>
2021-03-26 18:17:53 +0200

    Done

--- js/silder.js
+++ js/silder.js
@@ -7,28 +7,28 @@ let point3 = document.getElementById('point3');
 let point4 = document.getElementById('point4');
 
 //first slide
-point1.onclick = function(){
+point1.onclick = function () {
     slider.style.transform = 'translateX(0)';
     active.style.top = '0px';
     slider.style.background = 'url("../style/assets/img1.jpg")';
 };
 
 //second slide
-point2.onclick = function(){
+point2.onclick = function () {
     slider.style.transform = 'translateX(-25%)';
     active.style.top = '70px';
     slider.style.background = 'url("../style/assets/img1.jpg")';
 };
 
 //third slide
-point3.onclick = function(){
+point3.onclick = function () {
     slider.style.transform = 'translateX(-50%)';
     active.style.top = '140px';
     slider.style.background = 'url("../style/assets/img1.jpg")';
 };
 
 //fourth slide
-point4.onclick = function(){
+point4.onclick = function () {
     slider.style.transform = 'translateX(-75%)';
     active.style.top = '210px';
     slider.style.background = 'url("../style/assets/img1.jpg")';
@@ -38,22 +38,22 @@ let chr = -1;
 let txt = 'Travel is amazing...'
 let l = txt.length
 let p = "_"
-function changetext()
-{
-  chr++;
-  t = document.all["text"].innerHTML
-  tt = t.substring(0,t.length-p.length)
-  if(txt.charAt(chr)!="<") {
-      document.all["text"].innerHTML= tt + txt.charAt(chr) + p
-  }
-  else {
-      document.all["text"].innerHTML= tt + txt.substring(chr, chr+4) + p
-      chr = chr + 3
-  }
-  setTimeout("changetext()", 400)
-  }
+function changetext() {
+    chr++;
+    t = document.all["text"].innerHTML
+    tt = t.substring(0, t.length - p.length)
+    if (txt.charAt(chr) != "<") {
+        document.all["text"].innerHTML = tt + txt.charAt(chr) + p
+    }
+    else {
+        document.all["text"].innerHTML = tt + txt.substring(chr, chr + 4) + p
+        chr = chr + 3
+    }
+    setTimeout("changetext()", 250)
+}
+
 function str(num) {
-let s = ""
-for(i=0; i<num; i++)
-{ s = s + " " }
-return s }
+    let s = ""
+    for (i = 0; i < num; i++) { s = s + " " }
+    return s
+}
